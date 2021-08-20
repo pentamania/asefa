63a4bff0c1c6608d96ffc3d87b805aa6a3de6a2b
pentamania <tanigami7220@gmail.com>
2021-08-20 21:38:28 +0900

    add: Add babel transpiling feature
    Polyfilling with corejs3

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,13 @@
 import replace from '@rollup/plugin-replace'
 import typescript from 'rollup-plugin-typescript2'
+import babel from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 import license from 'rollup-plugin-license'
-import {
-  name,
-  version,
-  license as LICENSE,
-  author,
-  dependencies,
-} from './package.json'
+import { name, version, license as LICENSE, author } from './package.json'
 
 // Config
+const extensions = ['.ts']
 const noDeclarationFiles = { compilerOptions: { declaration: false } }
-const externals = dependencies ? Object.keys(dependencies) : []
 
 const banner = `/*!
  * ${name} v${version}
@@ -29,9 +24,21 @@ export default [
       file: `lib/${name}.js`,
       format: 'cjs',
     },
-    external: externals,
+    external: [/@babel\/runtime/],
     plugins: [
       typescript({ useTsconfigDeclarationDir: true }),
+      babel({
+        extensions,
+        plugins: [
+          [
+            '@babel/plugin-transform-runtime',
+            {
+              corejs: 3,
+            },
+          ],
+        ],
+        babelHelpers: 'runtime',
+      }),
       license({ banner: banner }),
     ],
   },
@@ -43,14 +50,27 @@ export default [
       file: `lib/${name}.esm.js`,
       format: 'esm',
     },
-    external: externals,
+    external: [/@babel\/runtime/],
     plugins: [
       typescript({ tsconfigOverride: noDeclarationFiles }),
+      babel({
+        extensions,
+        plugins: [
+          [
+            '@babel/plugin-transform-runtime',
+            {
+              corejs: 3,
+            },
+          ],
+        ],
+        babelHelpers: 'runtime',
+      }),
       license({ banner: banner }),
     ],
   },
 
   // UMD Dev
+  // TODO: Include polyfills for umd build
   {
     input: 'src/index.ts',
     output: {
@@ -58,9 +78,13 @@ export default [
       format: 'umd',
       name: `${name}`,
     },
-    external: externals,
     plugins: [
       typescript({ tsconfigOverride: noDeclarationFiles }),
+      babel({
+        extensions,
+        exclude: 'node_modules/**',
+        babelHelpers: 'bundled',
+      }),
       replace({
         'process.env.NODE_ENV': JSON.stringify('development'),
       }),
@@ -76,9 +100,14 @@ export default [
       format: 'umd',
       name: `${name}`,
     },
-    external: externals,
     plugins: [
       typescript({ tsconfigOverride: noDeclarationFiles }),
+      babel({
+        extensions,
+        exclude: 'node_modules/**',
+        skipPreflightCheck: true,
+        babelHelpers: 'bundled',
+      }),
       replace({
         'process.env.NODE_ENV': JSON.stringify('production'),
       }),
